4e01340d8541384dc42d674b73f104e100a8f9a3
santoshr1016 <santosh.ratnala@acquia.com>
2019-05-17 09:24:46 +0530

    Correct output

--- itsybitsy/TH/dbs.py
+++ itsybitsy/TH/dbs.py
@@ -8,19 +8,40 @@ if response.ok:
 out_dict = dict()
 
 for val in data.values():
-    for item in val:
-        for k, v in item.items():
-            print(k, "->", v)
-        print()
-
-
-for val in data.values():
     for v in val:
-        if "service_name" in v:
+        if "service" in v:
             if out_dict:
-                out_dict.get("service_name").append(v["service_name"])
+                out_dict.get("service").append(v["service"])
 
             else:
-                out_dict["service_name"] = []
+                out_dict["service"] = []
 
-print(out_dict)
+ll = out_dict.get('service')
+continents = ("us", "eu", "ap")
+ret_dict = dict()
+for item in ll:
+    s = item.split('-')
+    i = 0
+    while i < len(s):
+        if s[i] in continents:
+            service = s[:i]
+            region = s[i:]
+            region = "-".join(region)
+            if len(service) > 0:
+                service = "-".join(service)
+            if region in ret_dict:
+                ret_dict.get(region).append(service)
+            else:
+                ret_dict[region] = []
+            break
+        i += 1
+for k, v in ret_dict.items():
+    print(k, "->", v)
+
+
+
+
+
+
+
+
