e86649720ddd901dd5593a16584c92bff23aff6b
santoshr1016 <santosh.ratnala@acquia.com>
2019-06-07 14:21:46 +0530

    Simple code

--- itsybitsy/Rakutan/amex_2.py
+++ itsybitsy/Rakutan/amex_2.py
@@ -1,5 +1,16 @@
-def solution(pwd):
-    pass
+def solution():
+    n = input()
+    l = [int(num) for num in input().split(' ')]
+    for item in l:
+        for num in range(1, item+1):
+            if not num % 15:
+                print("FizzBuzz")
+            elif not num % 3:
+                print("Fizz")
+            elif not num % 5:
+                print("Buzz")
+            else:
+                print(num)
 
-s = []
-print(solution(s))
+
+print(solution())
